9c11a45cb0dd44fbec8b3f8b571db6047eb4259c
MinorSc <misolano1192@gmail.com>
2024-08-11 21:46:06 -0600

    Proyecto completado

--- Js/encriptador.js
+++ Js/encriptador.js
@@ -1,70 +1,57 @@
 
 //Variable para manipular el DOM
 const textArea = document.querySelector(".text-area");
-const mensaje = document.querySelector(".mensaje");
+const mensaje = document.querySelector(".mensaje__textarea");
 const copyButton = document.getElementById('copiar-btn');
-// const messageTextarea = document.querySelector('.mensaje');
-
-
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
-
+const mensajeTitulo = document.querySelector(".mensaje__titulo");
+const mensajeParrafo = document.querySelector(".mensaje__parrafo");
 
 //Función encriptar
 function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value)
-    mensaje.value = textoEncriptado
+    mensaje.value = textoEncriptado;
     textArea.value = "";
-    mensaje.style.backgroundImage = "none"
+    mensaje.style.backgroundImage = "none";
+    mensajeTitulo.style.display = "none";
+    mensajeParrafo.style.display = "none";
 }
 
 
-
 function encriptar(stringEncriptada) {
-    
-    let matrizCodigo = [[ "e", "enter", ], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringEncriptada = stringEncriptada.toLowerCase()
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
-        
     }   
-    return stringEncriptada
+    return stringEncriptada;
 }
-
 
 //Función de desencriptar
 function btnDesenncriptar() {
     const textoDesencriptado = desencriptar(textArea.value)
-    mensaje.value = textoDesencriptado
+    mensaje.value = textoDesencriptado;
     textArea.value = "";
-    
+    mensaje.style.backgroundImage = "none";
+    mensajeTitulo.style.display = "none";
+    mensajeParrafo.style.display = "none";
 }
 
-
-
 function desencriptar(stringDesencriptada) {
-    
-    let matrizCodigo = [[ "e", "enter", ], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    stringDesencriptada = stringDesencriptada.toLowerCase()
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (stringDesencriptada.includes(matrizCodigo[i][1])) {
             stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
         }
-        
     }   
-    return stringDesencriptada
+    return stringDesencriptada;
 }
 
 //Función de botón copiar 
-
 function BotonCopiar() {
     // Selecciona el contenido del área de texto
     mensaje.select();
@@ -80,12 +67,6 @@ function BotonCopiar() {
 }
 
 // Añade el evento de clic al botón de copiar
-copyButton.addEventListener('click', BotonCopiar)
-
-
-// //Pintar titulo y párrafo de mensaje 
-// function name(params) {
-    
-// }
+copyButton.addEventListener('click', BotonCopiar);
 
 
